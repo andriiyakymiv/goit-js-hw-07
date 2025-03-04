0fe438c9a4fdea437be3be2d07730f0d0e25f8c0
andrii <a.iakumiv@gmail.com>
2025-03-04 09:16:51 +0200

    Add styling for task four

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const formLogin = document.querySelector('.login-form');
 
+formLogin.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = {};
+  for (const element of formLogin.elements) {
+    if (element.name) {
+      formData[element.name] = element.value;
+    }
+  }
+  if (formData.email && formData.password) {
+    console.log(formData);
+    formLogin.reset();
+  } else {
+    alert('Всі поля повинні бути заповнені');
+  }
+});
